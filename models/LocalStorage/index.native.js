9616598b21ebd5265dd011b3a72237f55e777e69
azad <azad26195@gmail.com>
2016-10-28 13:34:23 +0530

    Localstorage model addded

---
+++ models/LocalStorage/index.native.js
@@ -0,0 +1,25 @@
+import Realm from 'realm';
+import Activity from '../Activity';
+import Monitor from '../Monitor';
+
+export let realm = new Realm({schema: [Monitor, Activity]});
+
+export default class LocalStorage {
+
+  save(key, value) {
+    realm.write(()=> {
+      realm.create(key, value);
+    })
+  }
+
+  read(key, q = null, sort = null) {
+    let ob = realm.objects(key);
+    if (q !== null) {
+      ob = ob.filtered(q)
+    }
+    if (sort !== null ){
+      ob = ob.sorted(sort);
+    }
+    return ob;
+  }
+}
