acc11c31b2831e980d7d64ac8a5b0bc1afa4cbf3
azad <azad26195@gmail.com>
2016-10-29 10:10:36 +0530

    Android native drawer used

--- index.android.js
+++ index.android.js
@@ -3,34 +3,41 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  DrawerLayoutAndroid
 } from 'react-native';
 import {
   SideMenu
 } from 'react-native-elements'
 
-import {
-  App,
-  MenuComponent
-} from './Components';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react/native';
+import App from './Components/App';
+import MenuComponent from './Components/MenuComponent';
+
+@observer
 export default class WorkMonitor extends Component {
   constructor () {
     super()
     this.state = { toggled: false }
+    this.toggleSideMenu = this.toggleSideMenu.bind(this);
   }
-
+  @observable toggle = false;
   toggleSideMenu () {
-    this.setState({
-      toggled: !this.state.toggled
-    })
+    this.toggle = !this.toggle;
   }
   render() {
+    const drawer = this.refs['drawer'];
+    (this.toggle)? drawer.openDrawer(): drawer.closeDrawer();
     return (
-      <SideMenu
-        MenuComponent={MenuComponent}
-        toggled={this.state.toggled}>
+      <DrawerLayoutAndroid
+        ref={'drawer'}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        drawerWidth={300}
+        renderNavigationView={()=><MenuComponent/>}
+        >
         <App toggleSideMenu={this.toggleSideMenu}/>
-      </SideMenu>
+      </DrawerLayoutAndroid>
     );
   }
 }
