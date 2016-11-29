31faa7bc1db014322d38ba709ced83c201c657a2
azad <azad26195@gmail.com>
2016-11-29 21:37:24 +0530

    chore(activity): List rendered

--- Components/App/index.native.js
+++ Components/App/index.native.js
@@ -1,15 +1,28 @@
 import React , { Component } from 'react';
+import {observer} from 'mobx-react/native';
 import {
   View,
   Text
 } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 import Activity from '../../models/Activity';
+
+@observer
 class App extends Component {
   render() {
-    const activity = Activity.activityList
+    const activity = Activity.activityList;
     return (
       <View style={{flex:1, paddingTop: 50}}>
-
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            Object.keys(activity).map((l) => (
+                <ListItem
+                  key={l}
+                  title={activity[l].name}
+                  />
+              ))
+          }
+        </List>
       </View>
     );
   }
