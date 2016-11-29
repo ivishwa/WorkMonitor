8949a7d4ed1b0a0de94813fdc9d6cc8d4f2ef58d
azad <azad26195@gmail.com>
2016-11-29 21:36:45 +0530

    chore(default activity added)

--- models/Activity.js
+++ models/Activity.js
@@ -2,16 +2,45 @@ import { observable, computed } from 'mobx';
 import LS from './LocalStorage';
 
 export class Activity {
-	@observable activityListCache = []
-
-	@computed get activityList() {
-		if(!this.activityListCache.length)
-			this.fetchActivity()
-		return this.activityListCache
+	constuctor(){
+		this.setDefaultActivity();
 	}
+	@observable activityList = LS.read('Activity');
 
-	fetchActivity(){
-		return Promise.resolve(() => this.activityListCache = LS.read('Activity') )
+	setDefaultActivity(){
+		let temp = LS.read('Activity')
+		let currentDate = new Date();
+		let defaultActivity = [{
+				name: 'Programming',
+				records: [
+					{
+						startTime: currentDate,
+						endTime: currentDate
+					}
+				]
+			},
+			{
+				name: 'Study',
+				records: [
+					{
+						startTime: currentDate,
+						endTime: currentDate
+					}
+				]
+			},
+			{
+				name: 'Exercise',
+				records: [
+					{
+						startTime: currentDate,
+						endTime: currentDate
+					}
+				]
+			}
+		]
+		if(temp.length == 0){
+			defaultActivity.map(val => LS.save('Activity', val))
+		}
 	}
 }
 
