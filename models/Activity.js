5e6fa508ba24c38208729b94374cfbb1fc0f7b0f
azad <azad26195@gmail.com>
2016-11-29 22:54:28 +0530

    activity add and update activity list created

--- models/Activity.js
+++ models/Activity.js
@@ -2,10 +2,26 @@ import { observable, computed } from 'mobx';
 import LS from './LocalStorage';
 
 export class Activity {
-	constuctor(){
-		this.setDefaultActivity();
+	@observable activityListCache = {};
+
+	@computed get activityList() {
+		if(Object.keys(this.activityListCache).length){
+			return this.activityListCache
+		}
+		else{
+			this.fetchResult()
+			return {}
+		}
 	}
-	@observable activityList = LS.read('Activity');
+
+	fetchResult(){
+		console.log('reached');
+		return Promise.resolve().then(()=>{
+			console.log("value", LS.read('Activity'))
+			this.setDefaultActivity();
+			return this.activityListCache = LS.read('Activity')
+		});
+	}
 
 	setDefaultActivity(){
 		let temp = LS.read('Activity')
@@ -41,6 +57,22 @@ export class Activity {
 		if(temp.length == 0){
 			defaultActivity.map(val => LS.save('Activity', val))
 		}
+	}
+
+	add(val){
+		let currentDate = new Date();
+		this.activityListCache = {}
+		return LS.save('Activity',
+			{
+				name: val,
+				records: [
+					{
+						startTime: currentDate,
+						endTime: currentDate
+					}
+				]
+			}
+		)
 	}
 }
 
