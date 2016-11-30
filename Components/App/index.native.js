df78de55ff37599b0dcf862a2372d8381059a70d
azad <azad26195@gmail.com>
2016-11-30 19:56:21 +0530

    refactor(app): uses Activity List component

--- Components/App/index.native.js
+++ Components/App/index.native.js
@@ -1,31 +1,19 @@
 import React , { Component } from 'react';
-import {observer} from 'mobx-react/native';
 import {
   View,
   Text
 } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 import Activity from '../../models/Activity';
-
+import { Actions } from 'react-native-router-flux';
 import ActionButton from 'react-native-action-button';
+import ActivityList from '../Activity/ActivityList';
 
-@observer
 class App extends Component {
   render() {
-    const activity = Activity.activityList;
     return (
       <View style={{flex:1, paddingTop: 50}}>
-        <List containerStyle={{marginBottom: 20}}>
-          {
-            Object.keys(activity).map((l) => (
-                <ListItem
-                  onPress={() => console.log("Hiee")}
-                  key={l}
-                  title={activity[l].name}
-                  />
-              ))
-          }
-        </List>
+        <ActivityList />
         <ActionButton
           buttonColor="rgba(231,76,60,1)"
           onPress={() => console.log("Hiee")}
