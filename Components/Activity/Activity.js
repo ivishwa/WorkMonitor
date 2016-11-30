a8bc49fc4d8c22b7f852136a2da03de9a23f06d4
azad <azad26195@gmail.com>
2016-11-30 20:04:35 +0530

    activty components displays selectedActivity

--- Components/Activity/Activity.js
+++ Components/Activity/Activity.js
@@ -3,11 +3,13 @@ import {
 	Text,
 	View
 } from 'react-native';
+import Activitys from '../../models/Activity';
+
 class Activity extends Component {
 	render() {
 		return (
 			<View style={{flex:1, paddingTop: 50}}>
-				<Text>React Native</Text>
+				<Text>{Activitys.selectedActivity}</Text>
 			</View>
 			);
 	}
