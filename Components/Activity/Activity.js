5b01a8a9cba3bd167837329616a861a79a984459
azad <azad26195@gmail.com>
2016-11-30 21:05:18 +0530

    start stop button added in activity

--- Components/Activity/Activity.js
+++ Components/Activity/Activity.js
@@ -3,13 +3,40 @@ import {
 	Text,
 	View
 } from 'react-native';
-import Activitys from '../../models/Activity';
 
+import {
+	ButtonGroup,
+	Button
+} from 'react-native-elements';
+import Activitys from '../../models/Activity';
+import {Actions} from 'react-native-router-flux';
+import {observer} from 'mobx-react/native';
+import LS from '../../models/LocalStorage';
+@observer
 class Activity extends Component {
+	constructor () {
+  		super()
+  		this.state = {
+    		isStarted: false
+  		}
+  	this.updateisStarted = this.updateisStarted.bind(this)
+	}
+	updateisStarted (selectedIndex) {
+  		const selectedActivity = LS.read('Activity',`name="${Activitys.selectedActivity.name}"`)
+  		console.log(selectedActivity['0'])
+	}
+	
 	render() {
+		const buttons = ['Start','End'];
+		const { selectedIndex } = this.state
 		return (
 			<View style={{flex:1, paddingTop: 50}}>
-				<Text>{Activitys.selectedActivity}</Text>
+				<Button
+				raised
+				backgroundColor={this.state.isStarted ? "red" :"#00303f"}
+				title={this.state.isStarted ? "Stop Activity" : "Start Activity"}
+				onPress={this.updateisStarted}
+				/>
 			</View>
 			);
 	}
