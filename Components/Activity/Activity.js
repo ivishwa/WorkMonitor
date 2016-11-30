7dc5e17d5fabffb2d075baebb88644653a81d140
azad <azad26195@gmail.com>
2016-11-30 19:54:29 +0530

    chore(activity): basic Component created

---
+++ Components/Activity/Activity.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+	Text,
+	View
+} from 'react-native';
+class Activity extends Component {
+	render() {
+		return (
+			<View style={{flex:1, paddingTop: 50}}>
+				<Text>React Native</Text>
+			</View>
+			);
+	}
+}
+
+export default Activity;
