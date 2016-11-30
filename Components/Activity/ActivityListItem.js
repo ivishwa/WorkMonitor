9e084c1083f4b0d09463eaa1047628a886efb994
azad <azad26195@gmail.com>
2016-11-30 19:55:45 +0530

    refactor(app): created ActivityListItem component

--- Components/Activity/ActivityListItem.js
+++ Components/Activity/ActivityListItem.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import { ListItem } from 'react-native-elements';
+import { Actions } from 'react-native-router-flux';
 
 class ActivityListItem extends Component {
-  render(){
-    return (
-
-    );
-  }
+	render(){
+		const { index, activity } = this.props;
+		return (
+			<ListItem
+				onPress={() => Actions.activity()}
+				title={activity.name}
+			/>
+		);
+	}
 }
 export default ActivityListItem;
