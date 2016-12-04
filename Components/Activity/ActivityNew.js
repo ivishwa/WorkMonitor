53af99a7704f918a96a882cc7434f4a09b6fc09f
azad <azad26195@gmail.com>
2016-12-04 21:18:57 +0530

    add button added

--- Components/Activity/ActivityNew.js
+++ Components/Activity/ActivityNew.js
@@ -4,14 +4,32 @@ import {
 	View,
 	Text
 } from 'react-native';
+
+import {
+ 	FormLabel, 
+ 	FormInput,
+ 	Button 
+} from 'react-native-elements'
+import Activitys from '../../models/Activity';
 class ActivityNew extends Component {
+
+	constructor(){
+		super()
+		this.state = {value: ""}
+	}
 	render() {
 		return (
 				<View style={{flex:1, paddingTop: 50}}>
-					<Text>NewPage</Text>
+					<FormLabel>Activity Name</FormLabel>
+					<FormInput onChangeText={(text) => this.setState({value: text})} />
+					<Button
+  						raised
+  						title='Submit'
+  						fontFamily='Comic Sans MS' 
+  						onPress={() => Activitys.add(this.state.value)}/>
 				</View>
 			)
 	}
 }
-
+	
 export default ActivityNew;
