6054811833324e91cd5e3f086a5b475c93b0af34
azad <azad26195@gmail.com>
2016-12-04 20:51:43 +0530

    newActivity screen add

---
+++ Components/Activity/ActivityNew.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import {
+	View,
+	Text
+} from 'react-native';
+class ActivityNew extends Component {
+	render() {
+		return (
+				<View style={{flex:1, paddingTop: 50}}>
+					<Text>NewPage</Text>
+				</View>
+			)
+	}
+}
+
+export default ActivityNew;
