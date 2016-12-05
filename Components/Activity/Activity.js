f77665bdee3fcf8cb3d276cd65deffc5f7b860d0
azad <azad26195@gmail.com>
2016-12-05 13:04:27 +0530

    start activity and end activity added

--- Components/Activity/Activity.js
+++ Components/Activity/Activity.js
@@ -11,30 +11,40 @@ import {
 import Activitys from '../../models/Activity';
 import {Actions} from 'react-native-router-flux';
 import {observer} from 'mobx-react/native';
+import {observable} from 'mobx';
+
 import LS from '../../models/LocalStorage';
 @observer
 class Activity extends Component {
 	constructor () {
   		super()
-  		this.state = {
-    		isStarted: false
-  		}
-  	this.updateisStarted = this.updateisStarted.bind(this)
+  		this.updateisStarted = this.updateisStarted.bind(this)
 	}
-	updateisStarted (selectedIndex) {
+	updateisStarted () {
   		const selectedActivity = LS.read('Activity',`name="${Activitys.selectedActivity.name}"`)
-  		console.log(selectedActivity['0'])
+  		if(selectedActivity['0'].isStarted == false){
+			LS.update((res) => {
+				res['0'].isStarted = true
+				res['0'].records.push({startTime: new Date(), endTime: new Date() })
+			}, [selectedActivity]);
+
+  		} else {
+  			LS.update((res) => {
+				res['0'].isStarted = false
+				let lastItem = res['0'].records[res['0'].records.length - 1] 
+				lastItem.endTime = new Date();
+			}, [selectedActivity]);
+  		}
 	}
 	
 	render() {
-		const buttons = ['Start','End'];
-		const { selectedIndex } = this.state
+		const selectedActivity = LS.read('Activity',`name="${Activitys.selectedActivity.name}"`)
 		return (
 			<View style={{flex:1, paddingTop: 50}}>
 				<Button
 				raised
-				backgroundColor={this.state.isStarted ? "red" :"#00303f"}
-				title={this.state.isStarted ? "Stop Activity" : "Start Activity"}
+				backgroundColor={selectedActivity['0'].isStarted ? "red" :"#00303f"}
+				title={selectedActivity['0'].isStarted ? "Stop Activity" : "Start Activity"}
 				onPress={this.updateisStarted}
 				/>
 			</View>
