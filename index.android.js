8f58f0634d525d929f3573cc6ccccf19917ea8d5
azad <azad26195@gmail.com>
2016-12-06 13:00:07 +0530

    scene consist of Report component

--- index.android.js
+++ index.android.js
@@ -15,6 +15,7 @@ import {observer} from 'mobx-react/native';
 import App from './Components/App';
 import Activity from './Components/Activity/Activity';
 import ActivityNew from './Components/Activity/ActivityNew';
+import Report from './Components/Reports';
 
 import MenuComponent from './Components/MenuComponent';
 import menuButton from './Components/NavigationItems';
@@ -44,6 +45,7 @@ export default class WorkMonitor extends Component {
             <Scene key="app" component={App} title="Activity" initial={true} toggleSideMenu={this.toggleSideMenu}/>
             <Scene key="activity" component={Activity} title="Activity" toggleSideMenu={this.toggleSideMenu}/>
             <Scene key="activityNew" component={ActivityNew} title="New" toggleSideMenu={this.toggleSideMenu}/>
+            <Scene key="report" component={Report} title="Report" toggleSideMenu={this.toggleSideMenu}/>
           </Scene>
         </Router>
       </DrawerLayoutAndroid>
